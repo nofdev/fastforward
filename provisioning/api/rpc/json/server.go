139b8c46c5dbb88be5c41b0b15a5a076f9e2fc4d
jiasir <taio@outlook.com>
2015-11-24 19:12:47 +0800

    Using compose instead of MakeConfig

--- provisioning/api/rpc/json/server.go
+++ provisioning/api/rpc/json/server.go
@@ -4,15 +4,13 @@ import (
     "github.com/gorilla/mux"
     "github.com/gorilla/rpc/v2"
     "github.com/gorilla/rpc/v2/json"
-    "log"
+    // "log"
     "net/http"
     "github.com/nofdev/fastforward/provisioning"
 )
 
-// Provisioning contains the configuration of ssh.
-type Provisioning struct {
-    provisioning.Conf
-}
+// Provisioning for API.
+type Provisioning struct {}
 
 // File contains filename for GetFile and PutFile.
 type File struct {
@@ -31,28 +29,18 @@ type Result interface {}
 
 // Exec takes a command to be executed from API on the remote server.
 func (p *Provisioning) Exec(r *http.Request, args *Args, result *Result) error {
-	c, err := provisioning.MakeConfig(args.User, args.Host, args.DisplayOutput, args.AbortOnError); if err != nil {
-		log.Printf("Make config error, %s", err)
-	}
-    
 	cmd := provisioning.Cmd{AptCache: args.AptCache, UseSudo: args.UseSudo, CmdLine: args.CmdLine}
-
-	i := provisioning.Provisioning(c)
+	i := provisioning.Provisioning(args)
 	*result, _ = i.Execute(cmd)
     return nil
 }
 
 // GetFile copies the file from the remote host to the local FastForward server, using scp. Wildcards are not currently supported. 
 func (p *Provisioning) GetFile(r *http.Request, args *Args, result *Result) error {
-    c, err := provisioning.MakeConfig(args.User, args.Host, args.DisplayOutput, args.AbortOnError); if err != nil {
-        log.Printf("Make config error, %s", err)
-    }
-    
     if args.RemoteFile== "" || args.LocalFile == "" {
         *result = "RemoteFile or LocalFile are needed."
     }
-
-    i := provisioning.Provisioning(c)
+    i := provisioning.Provisioning(args)
     *result = i.GetFile(args.RemoteFile, args.LocalFile)
     return nil
 }
