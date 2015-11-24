848d775617034a42f8d0e9dd2d4394f37ff61dd9
jiasir <taio@outlook.com>
2015-11-24 10:41:17 +0800

    Add jsonrpctest util

---
+++ tools/jsonrpctest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import urllib2
+import json
+import sys
+import yaml
+
+
+def rpc_call(url, method, args):
+    data = json.dumps({
+        'id': 1,
+        'method': method,
+        'params': [args]
+    }).encode()
+    req = urllib2.Request(url,
+        data,
+        {'Content-Type': 'application/json'})
+    f = urllib2.urlopen(req)
+    response = f.read()
+    return json.loads(response)
+
+url = sys.argv[1]
+method = sys.argv[2]
+args = yaml.load(sys.argv[3])
+print rpc_call(url, method, args)
