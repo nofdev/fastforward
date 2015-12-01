2f1d8b9c523758e1aa4edd8121f1268bcf945d56
jiasir <taio@outlook.com>
2015-12-01 14:12:24 +0800

    Playback API supported

--- provisioning/api/rpc/json/openstack/openstack.go
+++ provisioning/api/rpc/json/openstack/openstack.go
@@ -24,3 +24,230 @@ func (o *OpenStack) ConfigureStorageNetwork(r *http.Request, args *Args, result 
 	*result = i.ConfigureStorageNetwork()
 	return nil
 }
+
+// TODO: Args comment.
+// TODO: Return value of errors.
+
+// LoadBalancer deploy a HAProxy and Keepalived for OpenStack HA.
+func (o *OpenStack) LoadBalancer(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.LoadBalancer()
+	return nil
+}
+
+// LBOptimize optimizing load balancer.
+func (o *OpenStack) LBOptimize(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.LBOptimize()
+	return nil
+}
+
+// PrepareBasicEnvirionment prepares OpenStack basic environment.
+func (o *OpenStack) PrepareBasicEnvirionment(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.PrepareBasicEnvirionment()
+	return nil
+}
+
+// MariadbCluster deploy MariaDB Cluster.
+func (o *OpenStack) MariadbCluster(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.MariadbCluster()
+	return nil
+}
+
+// RabbtmqCluster deploy RabbitMQ Cluster.
+func (o *OpenStack) RabbtmqCluster(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.RabbtmqCluster()
+	return nil
+}
+
+// Keystone method deploy the Keystone components.
+func (o *OpenStack) Keystone(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.Keystone()
+	return nil
+}
+
+// FormatDiskForSwift formats devices for Swift Storage (sdb1 and sdc1).
+func (o *OpenStack) FormatDiskForSwift(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.FormatDiskForSwift()
+	return nil
+}
+
+// SwiftStorage deploy Swift storage.
+func (o *OpenStack) SwiftStorage(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.SwiftStorage()
+	return nil
+}
+
+// SwiftProxy deploy Swift proxy HA.
+func (o *OpenStack) SwiftProxy(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.SwiftProxy()
+	return nil
+}
+
+// InitSwiftRings initial Swift rings.
+func (o *OpenStack) InitSwiftRings(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.InitSwiftRings()
+	return nil
+}
+
+// DistSwiftRingConf destribute Swift ring configuration files.
+func (o *OpenStack) DistSwiftRingConf(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.DistSwiftRingConf()
+	return nil
+}
+
+// FinalizeSwift finalize Swift installation.
+func (o *OpenStack) FinalizeSwift(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.FinalizeSwift()
+	return nil
+}
+
+// Glance deploy Glance HA.
+func (o *OpenStack) Glance(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.Glance()
+	return nil
+}
+
+// CephAdmin deploy the Ceph admin node.
+func (o *OpenStack) CephAdmin(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.CephAdmin()
+	return nil
+}
+
+// CephInitMon deploy the Ceph initial monitor.
+func (o *OpenStack) CephInitMon(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.CephInitMon()
+	return nil
+}
+
+// CephClient deploy the Ceph client.
+func (o *OpenStack) CephClient(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.CephClient()
+	return nil
+}
+
+// GetCephKey add Ceph initial monitors and gather the keys.
+func (o *OpenStack) GetCephKey(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.GetCephKey()
+	return nil
+}
+
+// AddOSD add the Ceph OSDs.
+func (o *OpenStack) AddOSD(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.AddOSD()
+	return nil
+}
+
+// AddCephMon add the Ceph monitors.
+func (o *OpenStack) AddCephMon(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.AddCephMon()
+	return nil
+}
+
+// SyncCephKey copy the Ceph keys to nodes.
+func (o *OpenStack) SyncCephKey(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.SyncCephKey()
+	return nil
+}
+
+// CephUserPool creates the cinder ceph user and pool name.
+func (o *OpenStack) CephUserPool(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.CephUserPool()
+	return nil
+}
+
+// CinderAPI deploy cinder-api.
+func (o *OpenStack) CinderAPI(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.CinderAPI()
+	return nil
+}
+
+// CinderVolume deploy cinder-volume on controller node(ceph backend).
+func (o *OpenStack) CinderVolume(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.CinderVolume()
+	return nil
+}
+
+// RestartCephDeps restart volume service dependency to take effect for ceph backend.
+func (o *OpenStack) RestartCephDeps(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.RestartCephDeps()
+	return nil
+}
+
+// NovaController deploy Nova controller.
+func (o *OpenStack) NovaController(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.NovaController()
+	return nil
+}
+
+// Dashboard deploy Horizon.
+func (o *OpenStack) Dashboard(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.Dashboard()
+	return nil
+}
+
+// NovaComputes deploy Nova computes.
+func (o *OpenStack) NovaComputes(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.NovaComputes()
+	return nil
+}
+
+// NovaNetwork deploy legacy networking nova-network(FLATdhcp Only).
+func (o *OpenStack) NovaNetwork(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.NovaNetwork()
+	return nil
+}
+
+// Heat deploy orchestration components(heat).
+func (o *OpenStack) Heat(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.Heat()
+	return nil
+}
+
+// AutoStart fix the service can not auto start when sys booting.
+func (o *OpenStack) AutoStart(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.AutoStart()
+	return nil
+}
+
+// Designate deploy DNS as a Service.
+func (o *OpenStack) Designate(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.Designate()
+	return nil
+}
+
+// KvmToDocker converts kvm to docker(OPTIONAL).
+func (o *OpenStack) KvmToDocker(r *http.Request, args *Args, result *Result) error {
+	i := provisioning.OpenStack(args)
+	*result = i.KvmToDocker()
+	return nil
+}
