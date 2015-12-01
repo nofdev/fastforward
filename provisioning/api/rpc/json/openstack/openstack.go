7d966c0c67ff683101a35ccbd470daec7a1f6062
jiasir <taio@outlook.com>
2015-12-01 12:41:21 +0800

    Split openstack package

--- provisioning/api/rpc/json/openstack/openstack.go
+++ provisioning/api/rpc/json/openstack/openstack.go
@@ -1,12 +1,8 @@
-package main
+package openstack
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/gorilla/rpc/v2"
-	"github.com/gorilla/rpc/v2/json"
 	"github.com/nofdev/fastforward/provisioning"
 )
 
@@ -26,18 +22,4 @@ type Result interface{}
 func (o *OpenStack) ConfigureStorageNetwork(r *http.Request, args *Args, result *Result) error {
 	*result = args.ExtraVars.ConfigureStorageNetwork()
 	return nil
-}
-
-func main() {
-	s := rpc.NewServer()
-	log.Printf("Playback API started")
-	s.RegisterCodec(json.NewCodec(), "application/json")
-	openstack := new(OpenStack)
-	s.RegisterService(openstack, "")
-	log.Printf("Register OpenStack service")
-	r := mux.NewRouter()
-	r.Handle("/v1", s)
-	log.Printf("Handle API version 1")
-	log.Printf("Listen on port 7001")
-	http.ListenAndServe(":7001", r)
 }
---
+++ provisioning/api/rpc/json/openstack/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/rpc/v2"
+	"github.com/gorilla/rpc/v2/json"
+	"github.com/nofdev/fastforward/provisioning/api/rpc/json/openstack"
+)
+
+
+func main() {
+	s := rpc.NewServer()
+	log.Printf("Playback API started")
+	s.RegisterCodec(json.NewCodec(), "application/json")
+	openstack := new(openstack.OpenStack)
+	s.RegisterService(openstack, "")
+	log.Printf("Register OpenStack service")
+	r := mux.NewRouter()
+	r.Handle("/v1", s)
+	log.Printf("Handle API version 1")
+	log.Printf("Listen on port 7001")
+	http.ListenAndServe(":7001", r)
+}
