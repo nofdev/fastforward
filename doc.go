506998047e12efc162cdc484e716f5e49ce070ba
jiasir <taio@outlook.com>
2015-12-08 14:19:04 +0800

    Add godoc

---
+++ doc.go
@@ -0,0 +1,11 @@
+/*
+Copyright 2015 nofdev.
+Licensed under the GPLv2, see LICENCE file for details.
+
+FastForward is a DevOps automate platform.
+
+Project homepage: https://github.com/nofdev/fastforward
+
+For more information please refer to the README file in this directory.
+*/
+package fastforward
