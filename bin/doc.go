4d40bd6d184dcfd489668e761c85a904ee9e321e
jiasir <taio@outlook.com>
2015-12-08 14:35:17 +0800

    Add godoc

---
+++ bin/doc.go
@@ -0,0 +1,12 @@
+/*
+Package bin is the prebuild binary for FastForward.
+	For Linux x64 platform:
+	linux_amd64
+	
+	For Windows x64 platform:
+	windows_amd64
+	
+	For Mac OS X platform:
+	darwin_amd64
+*/
+package bin
