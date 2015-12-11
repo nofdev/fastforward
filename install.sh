cbf1e8bd754c08b88b96f2c7b64357efc008b4d2
jiasir <taio@outlook.com>
2015-12-11 19:33:38 +0800

    Install bin

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 go get -u -v github.com/nofdev/fastforward
-mkdir /etc/fastforward
-cp $GOPATH/src/github.com/nofdev/fastforward/config/fastforward.conf /etc/fastforward
+sudo mkdir /etc/fastforward
+sudo cp $GOPATH/src/github.com/nofdev/fastforward/config/fastforward.conf /etc/fastforward
+sudo chmod +x $GOPATH/src/github.com/nofdev/fastforward/bin/linux_amd64/*
+sudo cp -r $GOPATH/src/github.com/nofdev/fastforward/bin/linux_amd64/* /usr/local/bin
