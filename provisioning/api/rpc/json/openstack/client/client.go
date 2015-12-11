f267b16c0f6c7a29241408911e579d3a12cd2df3
jiasir <taio@outlook.com>
2015-12-11 15:29:47 +0800

    Update playback sdk

--- provisioning/api/rpc/json/openstack/client/client.go
+++ provisioning/api/rpc/json/openstack/client/client.go
@@ -1,1 +1,50 @@
 package client
+
+import (
+	"net/http"
+	"log"
+	"bytes"
+	
+	"github.com/gorilla/rpc/v2/json"
+	"github.com/nofdev/fastforward/provisioning/api/rpc/json/openstack"
+	"github.com/nofdev/fastforward/provisioning"
+)
+
+var url string
+var args *openstack.Args
+var method string
+var result openstack.Result
+
+func checkErr(err error) error{
+	if err !=nil {
+		log.Printf("%s", err)
+		return err
+	}
+	return nil
+}
+
+// Do holds a JSON-RPC request.
+func Do(url, method string, args *openstack.Args) error {
+	message, err := json.EncodeClientRequest(method, args)
+	checkErr(err)
+	
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(message))
+	checkErr(err)
+	req.Header.Set("Content-Type", "application/json")
+	
+	client := new(http.Client)
+	resp, err := client.Do(req); if err != nil {
+		log.Printf("Error in sending request to %s. %s", url, err)
+		return err
+	}
+	defer resp.Body.Close()
+	
+	err = json.DecodeClientResponse(resp.Body, &result)
+	if err != nil {
+		log.Printf("Couldn't decode response. %s", err)
+		return err
+	}
+	log.Printf("url: %s, method: %s, args: %s", url, method, args)
+	
+	return nil
+}
--- provisioning/api/rpc/json/openstack/client/client_test.go
+++ provisioning/api/rpc/json/openstack/client/client_test.go
@@ -1,1 +1,19 @@
 package client
+
+import (
+	"testing"
+
+	"github.com/nofdev/fastforward/provisioning/api/rpc/json/openstack"
+	"github.com/nofdev/fastforward/provisioning"
+)
+
+
+func TestDo(t *testing.T) {
+	url = "http://localhost:7001/v1"
+	args = &openstack.Args{provisioning.ExtraVars{HostName: "localhost", RouterID: "51", State: "Master", Priority: "50"}}
+	method = "OpenStack.LoadBalancer"
+	err := Do(url, method, args)
+	if err != nil {
+		t.Error(err)
+	}
+}
