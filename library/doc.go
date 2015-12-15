fba68b3b74fcedabcd0e24a9132169e8660537e4
jiasir <taio@outlook.com>
2015-12-15 20:31:56 +0800

    Adjust package name

--- library/doc.go
+++ library/doc.go
@@ -1,9 +1,9 @@
 /*
-Package libary are ansible modules for provision OpenStack. 
+Package library are ansible modules for provision OpenStack.
 Testing Modules:
 	git clone git://github.com/ansible/ansible.git --recursive
 	source ansible/hacking/env-setup
 	chmod +x ansible/hacking/test-module
 	ansible/hacking/test-module -m ./example -a "time=\"March 14 12:23\""
 */
-package libary
+package library
