2a7a7d2070adcfbda29f198d417cd41728565f3a
jiasir <taio@outlook.com>
2015-12-17 10:53:11 +0800

    Update test

--- library/common/network_test.go
+++ library/common/network_test.go
@@ -3,11 +3,10 @@ package common
 import "testing"
 
 func TestPurgeMainConf(t *testing.T) {
-	i := &Interfaces{"eth0", "0.0.0.0", "255.255.255.255", "0.0.0.1", "eth1", true}
 	i.PurgeMainConf()
 }
 
 func TestParseTmpl(t *testing.T) {
-	i := &Interfaces{"eth0", "0.0.0.0", "255.255.255.255", "0.0.0.1", "eth1", true}
+	i := &Interfaces{"eth0", "0.0.0.0", "255.255.255.255", "0.0.0.1", "0.0.0.2", "0.0.0.3", "eth1", true}
 	ParseTmpl(i, EtcNetworkInterface, "interface", "test1.txt", 0644)
 }
